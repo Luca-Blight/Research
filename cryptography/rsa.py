23b12b9b676633543886da4473fc70ec91a93225
zachary_royals <royalszachary@gmail.com>
2023-10-21 13:51:31 -0400

    circom added

--- cryptography/rsa.py
+++ cryptography/rsa.py
@@ -20,12 +20,13 @@ def extended_euclidean(a, b):
 # To create a RSA key pair, first randomly pick the two prime numbers to obtain the maximum (max).
 prime_one = 13
 prime_two = 7
-n = prime_one * prime_two  # Max number that can be encrypted
+max_num = prime_one * prime_two  # Max number that can be encrypted
 
 # Euler's totient function
 phi_n = (prime_one - 1) * (prime_two - 1)
 
 # Then pick a number to be the public key pub. As long as you know the two prime numbers, you can compute a corresponding private key priv from this public key.
+
 # Public key
 pub_key = 5
 
@@ -43,29 +44,28 @@ else:
     print("Public key is not valid.")
 
 
-def encrypt(number, pub_key, n):
+def encrypt(number, pub_key, max_num):
     print(f"original number: {number}")
 
-    # encrypted_number = pow(number, pub_key, n)
-    encrypted_number = (number**pub_key) % 91
+    encrypted_number = (number**pub_key) % max_num
     print(f"encrypted number is: {encrypted_number} ")
     return encrypted_number
 
 
-def decrypt(encrypted_number, private_key, n):
-    decrypted_number = (encrypted_number**private_key) % 91
+def decrypt(encrypted_number, private_key, max_num):
+    decrypted_number = (encrypted_number**private_key) % max_num
     print(f"decrypted number is: {decrypted_number}")
     return decrypted_number
 
 
-encrypted_number = encrypt(4, pub_key, n)
-descrypted_number = decrypt(encrypted_number, private_key, n)
+encrypted_number = encrypt(4, pub_key, max_num)
+descrypted_number = decrypt(encrypted_number, private_key, max_num)
 
 text = "CLOUD"
 numbers = [ord(char) for char in text]
 print("UTF-8 representation of CLOUD:", numbers)
-encrypted_message = [encrypt(num, pub_key, n) for num in numbers]
+encrypted_message = [encrypt(num, pub_key, max_num) for num in numbers]
 print("Encrypted Message:", encrypted_message)
 
-decrypted_message = [decrypt(num, private_key, n) for num in numbers]
-print("decrypted Message:", decrypted_message)
+decrypted_message = [decrypt(num, private_key, max_num) for num in numbers]
+print("Decrypted Message:", decrypted_message)
